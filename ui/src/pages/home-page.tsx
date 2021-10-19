d2f09635503093fccf1a18eee8fddcf27859e266
Mufaddal <mufaddal.kanchwala@datortech.com>
2021-10-19 20:45:38 +0530

    Fixed Design issues width height and position of components

--- ui/src/pages/home-page.tsx
+++ ui/src/pages/home-page.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Row, Col } from 'react-bootstrap';
+import { Row, Col, Container } from 'react-bootstrap';
 
 import { CategoryList, HomeBanner, ImageCarousel, ProductTile } from '../components/layout';
 
@@ -7,19 +7,21 @@ export const HomePage: React.FC = ():React.ReactElement => {
     return (
         <>
             <HomeBanner />
-            <Row>
-                <Col lg={3} sm={12}>
-                    <CategoryList />
-                </Col>
-                <Col lg={9} sm={12}>
-                    <ImageCarousel />
-                </Col>
-                <Col lg={3}>
-                </Col>
-                <Col lg={9} sm={12}>
-                    <ProductTile />
-                </Col>
-            </Row>
+            <Container>
+                <Row>
+                    <Col lg={3} sm={12}>
+                        <CategoryList />
+                    </Col>
+                    <Col lg={9} sm={12}>
+                        <ImageCarousel />
+                    </Col>
+                    <Col lg={3}>
+                    </Col>
+                    <Col lg={9} sm={12}>
+                        <ProductTile />
+                    </Col>
+                </Row>
+            </Container>
         </>
     );
 };
