bc1ec3496c2e20ae50d0f5d31af7c5fd743213d8
Ajay Nishad <ajay@datortech.com>
2021-10-22 05:11:15 +0530

    Facebook and google login implemented

--- node_api/src/auth/data_schema/index.js
+++ node_api/src/auth/data_schema/index.js
@@ -1,7 +1,10 @@
 const schema = require('./schema.auth');
-const googleSchema = require('./schema.google_auth')
+const googleSchema = require('./schema.google_auth');
+const facebookSchema = require('./schema.facebook_auth')
+
 
 module.exports = {
     schema,
-    googleSchema
+    googleSchema,
+    facebookSchema
 }
