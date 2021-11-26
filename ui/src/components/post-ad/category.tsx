94d17bab6b12097b1d43e93006c9a81b4b15c976
Mufaddal <mufaddal.kanchwala@datortech.com>
2021-11-26 22:00:13 +0530

    Ad post page design

--- ui/src/components/post-ad/category.tsx
+++ ui/src/components/post-ad/category.tsx
@@ -1,12 +1,10 @@
 import React, { useEffect } from 'react';
-import { Container, Row, Col, Form, FloatingLabel, Button } from 'react-bootstrap';
+import { Form, FloatingLabel } from 'react-bootstrap';
 import { useAxios } from '../../services/base-service';
 
-export const CategorySelector: React.FC = () => {
-    const [responseData, setResponseData] = React.useState<any>();
-    const [formSubmit, setFormSubmit] = React.useState<boolean>(false);
-    const [subCategoryData, setSubCategoryData] = React.useState<any>();
-    const [subCategorySelector, setSubCategorySelector] = React.useState<boolean>();
+export const CategorySelector: React.FC = (): React.ReactElement => {
+    const [responseData, setResponseData] = React.useState<any>([]);
+    const [subCategoryData, setSubCategoryData] = React.useState<any>([]);
 
     const [{ data: response, loading, error: apiError }, execute] = useAxios({
         url: '/category',
@@ -25,67 +23,48 @@ export const CategorySelector: React.FC = () => {
 
     const handleCategoryChange = (e: any) => {
         if (e.target.value) {
-            setSubCategorySelector(true);
-            setSubCategoryData({});
-            var index = responseData.findIndex((category: any) => category.name === e.target.value);
-            setSubCategoryData(responseData[index]?.subCategories);
+            var index = responseData.findIndex((category: any) => category.id === e.target.value);
+            console.log(index);
+            setSubCategoryData(responseData[index]?.subCategories[0].fields[0].values);
+            console.log(subCategoryData);
         }
     }
 
     const handleSubCategoryChange = (e: any) => {
         if (e.target.value) {
-            setFormSubmit(true);
         }
     };
 
     return (
-        <Container>
-            <Row className="g-2">
-                <Col md>
-                    <FloatingLabel label="Category" className="mb-3">
-                        <Form.Select onChange={handleCategoryChange}>
-                            <option>Select Category</option>
-                            {responseData && (
-                                <>
-                                    {console.log(responseData)}
-                                    {responseData?.map((category: any) => {
-                                        return (
-                                            <option key={category?.name} value={category?.name}>
-                                                {category?.name}
-                                            </option>
-                                        );
-                                    })}
-                                </>
-                            )}
-                        </Form.Select>
-                    </FloatingLabel>
-                </Col>
-                <Col md>
-                    {subCategorySelector && (
-                        <FloatingLabel label="Sub-Category">
-                            <Form.Select onChange={handleSubCategoryChange}>
-                                <option>Select Sub Category</option>
-                                <>
-                                    {subCategoryData?.map((category: any) => {
-                                        return (
-                                            <option key={category?.name} value={category?.name}>
-                                                {category?.name}
-                                            </option>
-                                        );
-                                    })}
-                                </>
-                            </Form.Select>
-                        </FloatingLabel>
+        <>
+            <FloatingLabel className="mb-3" controlId="floatingSelect" label="Select Category">
+                <Form.Select aria-label="Select Category" onChange={handleCategoryChange}>
+                    {responseData && (
+                        <>
+                            {responseData?.map((category: any) => {
+                                return (
+                                    <option key={category?.id} value={category?.id}>
+                                        {category?.name}
+                                    </option>
+                                );
+                            })}
+                        </>
                     )}
-                </Col>
-            </Row>
-            <Row>
-                <Col md>
-                    {formSubmit && (
-                        <Button>Submit</Button>
-                    )}
-                </Col>
-            </Row>
-        </Container>
+                </Form.Select>
+            </FloatingLabel>
+            <FloatingLabel className="mb-3" controlId="floatingSelect" label="Select Sub Category">
+                <Form.Select aria-label="Select Sub Category" onChange={handleSubCategoryChange}>
+                    <>
+                        {subCategoryData?.map((category: any) => {
+                            return (
+                                <option key={category?.name} value={category?.name}>
+                                    {category?.value}
+                                </option>
+                            );
+                        })}
+                    </>
+                </Form.Select>
+            </FloatingLabel>
+        </>
     );
 }
--- ui/src/components/post-ad/post-ad-form.tsx
+++ ui/src/components/post-ad/post-ad-form.tsx
@@ -1,12 +1,119 @@
 import React from 'react';
-import { CategorySelector } from './category';
-import './post-ad.css';
+import { Container, Row, Col, Button, InputGroup, FormControl, Form, FloatingLabel } from 'react-bootstrap';
+import ToggleButtonGroup from 'react-bootstrap/ToggleButtonGroup';
+import ToggleButton from 'react-bootstrap/ToggleButton';
+import Dropzone from 'react-dropzone';
+import { CategorySelector } from '.';
 
 export const PostAdForm: React.FC = (): React.ReactElement => {
-    return (
-        <div className="d-flex post-ad-box">
-            <h1 className="ad-post-text">Ad Post</h1>
-            <CategorySelector />
-        </div>
+    return ( 
+        <Container className="p-4 pt-lg-5">
+            <h1 className="mb-3 h2">What are you listing today?</h1>
+            <Row className="">
+                <Col className="col-lg-4 ">
+                    <div className="shadow p-3 p-lg-5 h-100">
+                        <Dropzone onDrop={acceptedFiles => console.log(acceptedFiles)}>
+                            {({ getRootProps, getInputProps }) => (
+                                <section>
+                                    <div {...getRootProps()}>
+                                        <input {...getInputProps()} />
+                                        <p>Drag 'n' drop some files here, or click to select files</p>
+                                    </div>
+                                </section>
+                            )}
+                        </Dropzone>
+                    </div>
+                </Col>
+                <Col className="col-lg-8">
+                    <div className="shadow p-3 p-lg-5 h-100">
+                        <CategorySelector />
+
+                        <FloatingLabel className="mb-5" controlId="floatingInputGrid" label="Listing Title">
+                            <Form.Control type="text" placeholder="Ad Title" />
+                        </FloatingLabel>
+
+                        <h5 className="mb-3">About the item</h5>
+                        <h6 className="text-muted">Condition</h6>
+                        <ToggleButtonGroup className="mb-5" type="radio" name="condition-options" defaultValue={1}>
+                            <ToggleButton id="tbg-radio-1" variant="outline-success fullround" className="rounded m-2 ms-0" value={1}>
+                                Brand New
+                            </ToggleButton>
+                            <ToggleButton id="tbg-radio-2" variant="outline-success fullround" className="rounded m-2" value={2}>
+                                Like New
+                            </ToggleButton>
+                            <ToggleButton id="tbg-radio-3" variant="outline-success fullround" className="rounded m-2" value={3}>
+                                Well used
+                            </ToggleButton>
+                            <ToggleButton id="tbg-radio-4" variant="outline-success fullround" className="rounded m-2" value={4}>
+                                Heavily used
+                            </ToggleButton>
+                        </ToggleButtonGroup>
+
+                        <h6 className="text-muted">Price</h6>
+                        <ToggleButtonGroup type="radio" name="price-options" defaultValue={1}>
+                            <ToggleButton id="price-radio-1" variant="outline-success fullround" className="rounded m-2 ms-0" value={5}>
+                                For Sale
+                            </ToggleButton>
+                            <ToggleButton id="price-radio-2" variant="outline-success fullround" className="rounded m-2" value={6}>
+                                For Free
+                            </ToggleButton>
+
+                        </ToggleButtonGroup>
+
+                        <InputGroup className="mt-2 mb-5">
+                            <InputGroup.Text id="basic-addon1">$</InputGroup.Text>
+                            <FormControl
+                                placeholder="Price of your listing"
+                                aria-label="Price of your listing"
+                                aria-describedby="basic-addon1"
+                            />
+                        </InputGroup>
+
+                        <h6 className="text-muted">Description</h6>
+
+                        <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+                            <Form.Control as="textarea" rows={5} placeholder="Describe what you are selling and include any details a buyer might be interested in. People love items with stories!" />
+                        </Form.Group>
+
+                        <FloatingLabel className="mb-5" controlId="floatingSelect" label="Brand">
+                            <Form.Select aria-label="Floating label select example">
+                                <option>Select Brand</option>
+                                <option value="1">One</option>
+                                <option value="2">Two</option>
+                                <option value="3">Three</option>
+                            </Form.Select>
+                        </FloatingLabel>
+
+                        <h5 className="mb-3">Optional details</h5>
+                        <p className="text-muted">With these details, buyers can find your listing more easily and ask fewer questions.</p>
+
+                        <h6 className="text-muted">Features</h6>
+                        <FloatingLabel className="mb-5" controlId="floatingSelect" label="Features">
+                            <Form.Select aria-label="Floating label select example">
+                                <option>Select Features</option>
+                                <option value="1">One</option>
+                                <option value="2">Two</option>
+                                <option value="3">Three</option>
+                            </Form.Select>
+                        </FloatingLabel>
+
+
+                        <h5 className="mb-3">Deal Method</h5>
+                        <div className="mb-5">
+                            <Form.Check label="Meet-up" />
+                            <Form.Check label="Delivery" />
+                        </div>
+
+                        <h5 className="mb-3">Payment methods</h5>
+                        <div className="mb-5">
+                            Coming Soon...
+                        </div>
+                        <div className="d-flex justify-content-end">
+                            <Button variant="fullround" className="btn-success rounded btn-lg">List Now</Button>
+                        </div>
+                    </div>
+                </Col>
+            </Row>
+        </Container>
     );
 };
