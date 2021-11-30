8407f918681975f44be8c80486ff07e11f82ae44
Mufaddal <mufaddal.kanchwala@datortech.com>
2021-11-30 19:20:28 +0530

    Added Form Validation and Ad details page with some minor fixes

---
+++ ui/src/components/ad-detail/ad-detail-description.tsx
@@ -0,0 +1,93 @@
+import React from 'react';
+import { Container, Row, Col, Button } from 'react-bootstrap';
+import { FaAsterisk, FaHandshake, FaMapMarkerAlt } from 'react-icons/fa';
+import { ProfileHeaderCard } from '../profile';
+import './ad-page.css';
+
+export const AdDetailDescription = () => {
+    return (
+        <Container>
+            <Row>
+                <Col className="col-lg-8 col-md-11 mx-auto">
+                    <h1 className="h2 text-dark mb-3">Polycom RealPresence Group 700 for sale @ $250 each(AAR660)</h1>
+                    <h2 className="text-success">$400</h2>
+                    <Row className="border-bottom py-3 mb-5">
+                        <Col>
+                            <FaAsterisk /> Used
+                                </Col>
+                        <Col>
+                            <FaHandshake /> Meetup
+                                </Col>
+                        <Col>
+                            <FaMapMarkerAlt /> Blk 2019 Bukit Batok St 23
+                                </Col>
+                    </Row>
+                    <Row>
+                        <Col className="col-12 mb-2">
+                            <h4>Description</h4>
+
+                        </Col>
+                        <Col className="col-12 mb-2">
+                            <p className="text-muted m-0">Posted</p>
+                            <p>2 weeks ago</p>
+                        </Col>
+
+                        <Col className="col-12 mb-2">
+                            <p>Please self inspect. Find just about anything using the app on your mobile.</p>
+                        </Col>
+                        <Col className="col-12 mb-2">
+                            <p>Qty: 1 pc</p>
+                        </Col>
+                        <Col className="col-12 mb-2">
+                            <p>Used. Items can be powered on, however, not fully tested.</p>
+                        </Col>
+                        <Col className="col-12 mb-2">
+                            <p> Model No : 700</p>
+                        </Col>
+
+                        <Col className="col-12 mb-2">
+                            <p>"First Come  First Serve ".<br />
+                                        Unable to reply inquiries, Therefore, JUST WALK IN to check the condition and see 5000 other items at our warehouse</p>
+
+                            <p>Thanks for your understanding. </p>
+
+                            <p>**********************************************<br />
+                                        Viewing & Self inspection: </p>
+
+                            <p>Blk 2019 Bukit Batok St 23 (Industrial Park A)<br />
+                                        #01-254, Singapore 659524</p>
+
+                            <p>Operating hours: 11 am to 5 pm (Mon - Sat).</p>
+                        </Col>
+
+                        <Col className="col-12 mb-2">
+                            <h4>Meet-up</h4>
+                            <p><FaMapMarkerAlt /> Blk 2019 Bukit Batok St 23</p>
+                        </Col>
+
+                        <Col className="col-12 mb-5">
+                            <h4>Payment</h4>
+
+                        </Col>
+                        <Col className="col-12 mb-2">
+                            <h4 className="mb-0">Meet the seller</h4>
+                            <ProfileHeaderCard />
+                        </Col>
+                    </Row>
+                </Col>
+                <Col lg={4} sm={12} className="d-flex">
+                    <div className="p-4">
+                        <h3>Buy and sell quickly, safely and locally!</h3>
+                        <p>Find just about anything using the app on your mobile.</p>
+                        <Button variant="link" className="p-0 me-lg-2">
+                            <img className="d-block mw-100" width="125" src="/images/appstore.png" alt="App Store" />
+                        </Button>
+                        <Button variant="link" className="p-0">
+                            <img className="d-block mw-100" width="125" src="/images/googleplay.png" alt="Google Play" />
+                        </Button>
+                    </div>
+                </Col>
+            </Row>
+        </Container>
+    );
+};
--- ui/src/components/post-ad/category.tsx
+++ ui/src/components/post-ad/category.tsx
@@ -1,10 +1,17 @@
 import React, { useEffect } from 'react';
 import { Form, FloatingLabel } from 'react-bootstrap';
 import { useAxios } from '../../services/base-service';
+import { useFormContext } from 'react-hook-form';
 
-export const CategorySelector: React.FC = (): React.ReactElement => {
+interface CategoryProps {
+    //data: any,
+};
+
+export const CategorySelector: React.FC<CategoryProps> = ({ }: CategoryProps): React.ReactElement => {
     const [responseData, setResponseData] = React.useState<any>([]);
     const [subCategoryData, setSubCategoryData] = React.useState<any>([]);
+
+    const { register, formState: { errors } } = useFormContext();
 
     const [{ data: response, loading, error: apiError }, execute] = useAxios({
         url: '/category',
@@ -21,12 +28,30 @@ export const CategorySelector: React.FC = (): React.ReactElement => {
         }
     }, [response]);
 
+    const getErrorText = (field: string): React.ReactElement | null => {
+        const errorMessages: any = {
+            ...errors
+        };
+        if (errorMessages[field]) {
+            return (
+                <Form.Text className="text-danger">
+                    {errorMessages[field]?.message}
+                </Form.Text>
+            );
+        }
+        return null;
+    };
+    const getErrorClassName = (field: string): string => {
+        const errorMessages: any = {
+            ...errors,
+        };
+        return errorMessages[field] ? 'is-invalid' : '';
+    };
+
     const handleCategoryChange = (e: any) => {
         if (e.target.value) {
             var index = responseData.findIndex((category: any) => category.id === e.target.value);
-            console.log(index);
             setSubCategoryData(responseData[index]?.subCategories[0].fields[0].values);
-            console.log(subCategoryData);
         }
     }
 
@@ -38,8 +63,8 @@ export const CategorySelector: React.FC = (): React.ReactElement => {
     return (
         <>
             <FloatingLabel className="mb-3" controlId="floatingSelect" label="Select Category">
-                <Form.Select aria-label="Select Category" onChange={handleCategoryChange}>
-                    <option>Select Category</option>
+                <Form.Select className={getErrorClassName('category')} aria-label="Select Category" {...register('category')} onChange={handleCategoryChange}>
+                    <option value="">Select Category</option>
                     {responseData && (
                         <>
                             {responseData?.map((category: any) => {
@@ -52,10 +77,11 @@ export const CategorySelector: React.FC = (): React.ReactElement => {
                         </>
                     )}
                 </Form.Select>
+                {getErrorText('category')}
             </FloatingLabel>
             <FloatingLabel className="mb-3" controlId="floatingSelect" label="Select Sub Category">
-                <Form.Select aria-label="Select Sub Category" onChange={handleSubCategoryChange}>
-                    <option>Select Sub Category</option>
+                <Form.Select className={getErrorClassName('subCategory')} aria-label="Select Sub Category" {...register('subCategory')} onChange={handleSubCategoryChange}>
+                    <option value="">Select Sub-Category</option>
                     <>
                         {subCategoryData?.map((category: any) => {
                             return (
@@ -66,6 +92,7 @@ export const CategorySelector: React.FC = (): React.ReactElement => {
                         })}
                     </>
                 </Form.Select>
+                {getErrorText('subCategory')}
             </FloatingLabel>
         </>
     );
