2e0cb04b8bd3700832d02049c0a0fed5d6b5c073
Mufaddal <mufaddal.kanchwala@datortech.com>
2021-12-03 18:23:48 +0530

    Fixes for Ad post page

--- ui/src/components/post-ad/drag-and-drop.tsx
+++ ui/src/components/post-ad/drag-and-drop.tsx
@@ -5,43 +5,43 @@ import './post-ad.css';
 
 export const DragAndDrop: React.FC = (): React.ReactElement => {
 
-    const [files, setFiles] = useState<any>([]);
-    const {getRootProps, getInputProps} = useDropzone({
-        accept: 'image/*',
-        onDrop: acceptedFiles => {
-          setFiles(acceptedFiles.map(file => Object.assign(file, {
-            preview: URL.createObjectURL(file)
-          })))
-        }
-    });
+  const [files, setFiles] = useState<any>([]);
+  const { getRootProps, getInputProps } = useDropzone({
+    accept: 'image/*',
+    onDrop: acceptedFiles => {
+      setFiles(acceptedFiles.map(file => Object.assign(file, {
+        preview: URL.createObjectURL(file)
+      })))
+    }
+  });
 
-    const thumbs = files.map((file: any) => (
-        <div key={file.name} className="thumb">
-          <div className="thumb-inner">
-            <img
-              src={file.preview}
-              className="img"
-            />
-          </div>
-        </div>
-      ));
-    
-    useEffect(() => {
-        // Make sure to revoke the data uris to avoid memory leaks
-        files.forEach((file: any) => URL.revokeObjectURL(file.preview));
-    },[files]);
+  const thumbs = files.map((file: any) => (
+    <div key={file.name} className="thumb">
+      <div className="thumb-inner">
+        <img
+          src={file.preview}
+          className="img"
+        />
+      </div>
+    </div>
+  ));
 
-    return(
-        <div className="shadow p-3 p-lg-5 h-100">
-            <div {...getRootProps({className: 'drag-n-drop'})}>
-                <BsBoxArrowUp style={{ width:"40px", height:"40px" }} />
-                <input {...getInputProps()} />
-                <p>Select or drag photos/videos here</p>
-                <p className="upload-limit-text">(Up to 10 files)</p>
-            </div>
-            <aside className="thumbs-container">
-                {thumbs}
-            </aside>
-        </div>
-    );
+  useEffect(() => {
+    // Make sure to revoke the data uris to avoid memory leaks
+    files.forEach((file: any) => URL.revokeObjectURL(file.preview));
+  }, [files]);
+
+  return (
+    <div className="shadow p-3 p-lg-5 h-100">
+      <div {...getRootProps({ className: 'drag-n-drop' })}>
+        <BsBoxArrowUp style={{ width: "40px", height: "40px" }} />
+        <input {...getInputProps()} />
+        <p>Select or drag photos/videos here</p>
+        <p className="upload-limit-text">(Up to 10 files)</p>
+      </div>
+      <aside className="thumbs-container">
+        {thumbs}
+      </aside>
+    </div>
+  );
 };
