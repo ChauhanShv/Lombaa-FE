2bec6929bfa9ab7bc4de15f0e536f8dc87b9a662
Ajay Nishad <ajay@datortech.com>
2021-12-04 18:27:44 +0530

    [API] Removed DeletedAt from response

--- laravel_admin/app/Http/Controllers/CategoryController.php
+++ laravel_admin/app/Http/Controllers/CategoryController.php
@@ -1,21 +1,22 @@
 <?php
 
 namespace App\Http\Controllers;
-use Illuminate\Support\Facades\Validator;
-use Illuminate\Http\Request;
+
 use App\Models\Category;
-use App\Models\Files;
-use App\Models\Fields;
 use App\Models\CategoryField;
-use Illuminate\Support\Facades\Storage;
+use App\Models\Fields;
+use App\Models\Files;
 use Carbon\Carbon;
-use Aws\S3\S3Client;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\Validator;
 use Str;
 
 class CategoryController extends Controller
 {
-    public function categories(Request $request){
-        if($request->isMethod('post')){
+    public function categories(Request $request)
+    {
+        if ($request->isMethod('post')) {
             $rules = [
                 'name' => 'required|regex:/^[\s\w-]*$/',
                 'description' => 'required',
@@ -23,16 +24,16 @@ class CategoryController extends Controller
                 'product' => 'required',
             ];
             $messages = [
-                    'name.required' => 'Name is required',
-                    'description.required' => 'Description is required',
-                    'image.required' => 'Image is required',
-                    'product.required'=> 'Product is required',
+                'name.required' => 'Name is required',
+                'description.required' => 'Description is required',
+                'image.required' => 'Image is required',
+                'product.required' => 'Product is required',
             ];
             $validator = Validator::make($request->all(), $rules, $messages);
             if ($validator->fails()) {
                 return redirect()->back()->withInput($request->all())->withErrors($validator);
             }
-            $image_name = Str::uuid().'.'.$request->file('image')->getClientOriginalName();
+            $image_name = Str::uuid() . '.' . $request->file('image')->getClientOriginalName();
             $path = Storage::disk('s3')->put('images', $request->image);
             $image_path = Storage::disk('s3')->url($path);
             $image_mime = $request->file('image')->getClientMimeType();
@@ -47,11 +48,11 @@ class CategoryController extends Controller
                 // 'absolute_path'=> $image_path,
                 'absolute_path' => 'https://lomba-task-temp.s3.ap-south-1.amazonaws.com/images/Djehr3diIDdqjo8Wf2JhFbZC70M9w1RZ0xb9VFH4.png',
                 'location' => 's3',
-                'createdAt'=> Carbon::now(),
-                'updatedAt' => Carbon::now()
+                'createdAt' => Carbon::now(),
+                'updatedAt' => Carbon::now(),
             ];
             $send_file_data = Files::insert($file_data);
-                
+
             $category_id = Str::uuid();
             $data = [
                 'id' => $category_id,
@@ -60,18 +61,16 @@ class CategoryController extends Controller
                 'iconId' => $file_data['id'],
                 'isPopular' => isset($request->popular) ? 1 : 0,
                 'isActive' => isset($request->active) ? 1 : 0,
-                'parentId' => $request->product ? $request->product  : null,
+                'parentId' => $request->product ? $request->product : null,
                 'createdAt' => Carbon::now(),
-                'updatedAt' => Carbon::now()
+                'updatedAt' => Carbon::now(),
             ];
 
             Category::insertGetId($data);
 
-            if($data) {
-
+            if ($data) {
                 $category_fields = array();
-
-                foreach($request->input('fields') as $field) {
+                foreach ($request->input('fields') as $field) {
                     $category_field = array();
 
                     $category_field['categoryId'] = $category_id;
@@ -81,39 +80,36 @@ class CategoryController extends Controller
 
                 $send_category_fields = CategoryField::insert($category_fields);
                 return redirect()->route('categories')->with('response', ['status' => 'success', 'message' => 'Categories added successfully']);
-            }
-            else{
+            } else {
                 return redirect()->route('categories')->with('response', ['status' => 'Failed', 'message' => 'Something went wrong']);
 
             }
 
-        }else{
-            
+        } else {
+
             $fields = Fields::get();
             $categories = Category::get();
-            return view('category.add', ['categories'=> $categories, 'fields'=> $fields]);
+            return view('category.add', ['categories' => $categories, 'fields' => $fields]);
         }
     }
 
-    public function category_list() {
+    public function category_list()
+    {
         $category_list = Category::with('icon')->paginate(30);
-        return view('category.categorylist',  ['category_list' => $category_list]);
+        return view('category.categorylist', ['category_list' => $category_list]);
     }
 
-
-    public function update_category($id) {
-    
+    public function update_category($id)
+    {
         $data = Category::with('icon')->find($id);
-
         $categories = Category::get();
         $fields = Fields::get();
 
-        return view('category.categoryupdate', ['data' => $data, 'categories' => $categories, 'fields'=> $fields]);
-
+        return view('category.categoryupdate', ['data' => $data, 'categories' => $categories, 'fields' => $fields]);
     }
 
-    
-    public function update_category_post (Request $request, $id) {
+    public function update_category_post(Request $request, $id)
+    {
 
         $rules = [
             'name' => 'required|regex:/^[\s\w-]*$/',
@@ -122,16 +118,16 @@ class CategoryController extends Controller
             'product' => 'required',
         ];
         $messages = [
-                'name.required' => 'Name is required',
-                'description.required' => 'Description is required',
-                'image.required' => 'Image is required',
-                'product.required'=> 'Product is required',
+            'name.required' => 'Name is required',
+            'description.required' => 'Description is required',
+            'image.required' => 'Image is required',
+            'product.required' => 'Product is required',
         ];
         $validator = Validator::make($request->all(), $rules, $messages);
         if ($validator->fails()) {
             return redirect()->back()->withInput($request->all())->withErrors($validator);
         }
-        $image_name = Str::uuid().'.'.$request->file('image')->getClientOriginalName();
+        $image_name = Str::uuid() . '.' . $request->file('image')->getClientOriginalName();
         $path = Storage::disk('s3')->put('images', $request->image);
         $image_path = Storage::disk('s3')->url($path);
         $image_mime = $request->file('image')->getClientMimeType();
@@ -146,16 +142,15 @@ class CategoryController extends Controller
             'name' => $image_name,
             'mime' => $image_mime,
             'relative_path' => '',
-            'absolute_path'=> $image_path,
+            'absolute_path' => $image_path,
             // 'absolute_path' => 'https://lomba-task-temp.s3.ap-south-1.amazonaws.com/images/Djehr3diIDdqjo8Wf2JhFbZC70M9w1RZ0xb9VFH4.png',
             'location' => 's3',
-            'createdAt'=> Carbon::now(),
-            'updatedAt' => Carbon::now()
+            'createdAt' => Carbon::now(),
+            'updatedAt' => Carbon::now(),
         ];
 
-
         $send_file_data = Files::where('id', $id)->update($file_data);
-            
+
         $category_id = Str::uuid();
         $data = [
             'name' => $request->name,
@@ -165,16 +160,16 @@ class CategoryController extends Controller
             'isActive' => isset($request->active) ? 1 : 0,
             'parentId' => $request->product,
             'createdAt' => Carbon::now(),
-            'updatedAt' => Carbon::now()
+            'updatedAt' => Carbon::now(),
         ];
 
         Category::insertGetId($data);
 
-        if($data) {
+        if ($data) {
 
             $category_fields = array();
 
-            foreach($request->input('fields') as $field) {
+            foreach ($request->input('fields') as $field) {
                 $category_field = array();
 
                 $category_field['categoryId'] = $category_id;
@@ -184,12 +179,10 @@ class CategoryController extends Controller
 
             $send_category_fields = CategoryField::insert($category_fields);
             return redirect()->route('categorylist')->with('response', ['status' => 'success', 'message' => 'Category updated successfully']);
-        }
-        else{
+        } else {
             return redirect()->route('categorylist')->with('response', ['status' => 'Failed', 'message' => 'Something went wrong']);
 
         }
     }
 
-    
 }
