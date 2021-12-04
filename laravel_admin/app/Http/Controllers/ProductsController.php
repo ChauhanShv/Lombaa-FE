0b6268b2b488fd302471ed442587180503f2b73a
Rishabh Shukla <rishabh@datortech.com>
2021-12-04 15:07:45 +0530

    Syntax error resolved

--- laravel_admin/app/Http/Controllers/ProductsController.php
+++ laravel_admin/app/Http/Controllers/ProductsController.php
@@ -1,25 +1,27 @@
 <?php
 
 namespace App\Http\Controllers;
+
 use App\Models\Products;
-use Illuminate\Http\Request;
 
 class ProductsController extends Controller
 {
-    public function products_list(){
+    public function products_list()
+    {
         $products = Products::paginate(30);
         return view('products.list', ['products' => $products]);
     }
 
-    public function approve_reject($action, $id) {
-        if($action == 'approve') {
-            $approve = [ 'is_Approved' => '1'];
+    public function approve_reject($action, $id)
+    {
+        if ($action == 'approve') {
+            $approve = ['is_Approved' => '1'];
             Products::where('id', $id)->update($approve);
-            return redirect()->route('products_list')->with('response', ['status' => 'success', 'message' => 'Approved'$id]);
-        }elseif ($action == 'reject') {
+            return redirect()->route('products_list')->with('response', ['status' => 'success', 'message' => 'Approved']);
+        } elseif ($action == 'reject') {
             $reject = ['is_Approved' => '0'];
             Products::where('id', $id)->update($reject);
-            return redirect()->route('products_list')->with('response', ['status' => 'success', 'message' => 'Rejected'$id]);
+            return redirect()->route('products_list')->with('response', ['status' => 'success', 'message' => 'Rejected']);
         }
     }
 }
