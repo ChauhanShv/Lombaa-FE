689d7d774d32131317cfccfb6465cb919c9b8ec3
Ajay Nishad <ajay@datortech.com>
2021-12-13 04:16:49 +0530

    Added product image upload API

---
+++ node_api/src/product/product_media.model.js
@@ -0,0 +1,25 @@
+const { Sequelize, DataTypes, Model } = require("sequelize");
+const sequelize = require("../modules/sequelize").service;
+const File = require("../file/file.model");
+
+class ProductMedia extends Model {}
+
+ProductMedia.init(
+  {
+    id: {
+      type: DataTypes.UUID,
+      primaryKey: true,
+      defaultValue: Sequelize.UUIDV4,
+    },
+  },
+  {
+    modelName: "ProductMedia",
+    tableName: "product_media",
+    sequelize,
+    paranoid: true,
+  }
+);
+
+ProductMedia.belongsTo(File, { as: "file" });
+
+module.exports = ProductMedia;
