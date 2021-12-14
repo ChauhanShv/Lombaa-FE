e3cb3d809944e7af56a3f17e7e44696a49eac3b1
Mufaddal <mufaddal.kanchwala@datortech.com>
2021-12-14 19:37:01 +0530

    Create Post Api Integrated

--- ui/src/components/create-post/create-post.tsx
+++ ui/src/components/create-post/create-post.tsx
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import { Container, Row, Col, Button, Form } from 'react-bootstrap';
+import React, { useEffect, useState } from 'react';
+import { Container, Row, Col, Button, Form, Spinner } from 'react-bootstrap';
 import { filter, isEmpty, isArray } from 'lodash';
 import {
     useForm,
@@ -29,6 +29,12 @@ export const CreatePost: React.FC<CreatePostProps> = ({
     categories,
 }: CreatePostProps): React.ReactElement => {
     const [selectedSubCategory, setSelectedSubCategory] = useState<SubCategories | null>(null);
+    const [filesToken, setFilesToken] = useState<Array<Object>>([]);
+    const [successData, setSuccessData] = useState<any>({
+        title: '',
+        description: '',
+        file: [],
+    });
     const customResolver = async (
         values: UnpackNestedValue<FieldValues>
     ): Promise<ResolverResult<FieldValues>> => {
@@ -55,7 +61,7 @@ export const CreatePost: React.FC<CreatePostProps> = ({
                         } else {
                             yupShape[field.id] = yup.string();
                         }
-                    break;
+                        break;
                     case 'checkbox':
                         yupShape[field.id] = yup.array().nullable().min(1, `${field.label} is required`);
                     default:
@@ -63,7 +69,7 @@ export const CreatePost: React.FC<CreatePostProps> = ({
                 }
             });
         }
-        const schema = yup.object().shape({...yupShape});
+        const schema = yup.object().shape({ ...yupShape });
         try {
             await schema.validate(values, { abortEarly: false });
             return Promise.resolve({
@@ -94,13 +100,31 @@ export const CreatePost: React.FC<CreatePostProps> = ({
         url: '/product',
         method: 'POST',
     });
+    const [{ data: mediaApi, loading: mediaLoading, error: mediaApiError }, executeMedia] = useAxios({
+        url: '/product/media',
+        method: 'POST',
+    });
+
+    useEffect(() => {
+        if (mediaApi?.Success && !filesToken.includes(mediaApi?.media?.token)) {
+            setFilesToken([...filesToken, { token: mediaApi?.media?.token }]);
+            if (filesToken.length === 0) {
+                setSuccessData({
+                    ...successData,
+                    file: mediaApi?.media?.url,
+                });
+            }
+        }
+    }, [mediaApi]);
+
     const onSubmit = (values: any) => {
         if (!isEmpty(errors)) {
             return false;
         }
         const postData: any = {
             categoryId: values?.subCategory,
-            fields: []
+            fields: [],
+            media: filesToken,
         };
         selectedSubCategory?.fields.forEach((field: Fields) => {
             const value: any = {};
@@ -113,8 +137,14 @@ export const CreatePost: React.FC<CreatePostProps> = ({
                 'title',
                 'label'
             ].includes(field.fieldType)) {
-                value.id = '';
+                value.id = null;
                 value.value = values[field.id];
+                if (field.fieldType === 'title') {
+                    setSuccessData({
+                        ...successData,
+                        title: values[field.id],
+                    });
+                }
             } else {
                 const selectedValue: CreatePostFieldValues = field.values.filter((item: CreatePostFieldValues) => {
                     if (isArray(values[field.id])) {
@@ -143,35 +173,52 @@ export const CreatePost: React.FC<CreatePostProps> = ({
     const onSubCategorySelected = (subCat: string) => {
         if (subCat) {
             const selectedSubCat = categories.map((cat: Categories) => {
-                return filter(cat.subCategories, {id: subCat});
+                return filter(cat.subCategories, { id: subCat });
             }).filter(i => i.length)[0] || [];
             setSelectedSubCategory(selectedSubCat[0] || {});
-        }else {
+        } else {
             setSelectedSubCategory(null);
         }
     };
 
-    return createPostRes?.status === 200 ? <ListingSuccessfulTile /> : (
+    const onFilesUpload = async (files: Array<Blob>) => {
+        const formData = new FormData();
+        for (const file of files) {
+            formData.append('media', file);
+            try {
+                await executeMedia({ data: formData });
+            } catch (error: any) { }
+            formData.delete('media');
+        }
+    }
+
+    return createPostRes?.Success ? <ListingSuccessfulTile {...successData} /> : (
         <Container className="p-4 pt-lg-5">
             <FormProvider {...formMethods}>
                 <Form onSubmit={handleFormSubmit} noValidate>
                     <h1 className="mb-3 h2">What are you listing today?</h1>
                     <Row>
-                        <Col lg={4}>
-                            <DragAndDrop />
+                        <Col lg={filesToken.length ? 4 : 12}>
+                            <DragAndDrop onFilesUpload={onFilesUpload} />
                         </Col>
-                        <Col lg={8}>
-                            <div className="shadow p-3 p-lg-5 h-100">
-                                <CategoryDropDown
-                                    categories={categories}
-                                    onSubCategorySelected={onSubCategorySelected}
-                                />
-                                {!!selectedSubCategory?.fields?.length && <FormFields fields={selectedSubCategory?.fields} />}
-                                <div className="d-flex justify-content-end">
-                                    <Button variant="fullround" className="btn-success rounded btn-lg" type="submit">List Now</Button>
+                        {!!filesToken.length && (
+                            <Col lg={8}>
+                                <div className="shadow p-3 p-lg-5 h-100">
+                                    <CategoryDropDown
+                                        categories={categories}
+                                        onSubCategorySelected={onSubCategorySelected}
+                                    />
+                                    {!!selectedSubCategory?.fields?.length && <FormFields fields={selectedSubCategory?.fields} />}
+                                    <div className="d-flex justify-content-end">
+                                        <Button variant="fullround" className="btn-success rounded btn-lg" type="submit">
+                                            {createPostLoading ? (
+                                                <Spinner animation="border" role="status"></Spinner>
+                                            ) : 'List Now'}
+                                        </Button>
+                                    </div>
                                 </div>
-                            </div>
-                        </Col>
+                            </Col>
+                        )}
                     </Row>
                 </Form>
             </FormProvider>
