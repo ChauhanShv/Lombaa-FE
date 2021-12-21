898bcae9d4800c22d2448a7f1526bfba512526f6
Rishabh Shukla <rishabh@datortech.com>
2021-12-21 19:05:31 +0530

    Id visibility on top removed from update of Country, region and city

--- laravel_admin/resources/views/location/region/update.blade.php
+++ laravel_admin/resources/views/location/region/update.blade.php
@@ -7,7 +7,7 @@
   <div class="span12">
     <div class="widget-box">
       <div class="widget-title"> <span class="icon"> <i class="icon-align-justify"></i> </span>
-        <h5>Update Region : {{ $region->name }} ({{ $region->id }})</h5>
+        <h5>Update Region : {{ $region->name }}</h5>
       </div>
       <div>
         @if (session('response'))
