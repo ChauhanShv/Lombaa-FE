ec876643089e89c61797ed509917bf651b19b5f0
Mufaddal <mufaddal.kanchwala@datortech.com>
2021-12-30 18:35:12 +0530

    Header And Settings Page Fixes

--- ui/src/config.ts
+++ ui/src/config.ts
@@ -5,4 +5,4 @@ export const REFRESH_TOKEN_RETRY = 1;
 export const FB_APPID = '643487683728427';
 export const GOOGLE_CLIENTID =
   '987010898284-ir8e6g3oe2ns2qqh2s58d1m7qe2usva8.apps.googleusercontent.com';
-export const CURRENT_COUNTRY = '73e7b3c9-52d4-11ec-809d-021b0ebe8bee'; // India
+export const CURRENT_COUNTRY = 'f7717d41-6012-4efa-bcfc-4f72c443cf28'; // Singapore
