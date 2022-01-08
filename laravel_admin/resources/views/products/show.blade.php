3711a3e4862568f1ca0f47e85a42073dcc6ebf38
Rishabh Shukla <rishabh@datortech.com>
2022-01-08 15:19:34 +0530

    Product view page changes

--- laravel_admin/resources/views/products/show.blade.php
+++ laravel_admin/resources/views/products/show.blade.php
@@ -19,27 +19,30 @@
         </div>
         <div class="widget-content">
             <div class="control-group">
-                <label class="control-label">Posted at : <strong>{{ $data->product->postedAt ?? '' }}</strong></label>
+                <label class="control-label">Posted at : <strong>{{ $product_data->postedAt ?? '' }}</strong></label>
             </div>
             <div class="control-group">
-                <label class="control-label">Approved at : <strong>{{ $data->product->approvedAt ?? '' }}</strong></label>
+                <label class="control-label">Approved at : <strong>{{ $product_data->approvedAt ?? '' }}</strong></label>
             </div>
             <div class="control-group">
-                <label class="control-label">Expiry at : <strong>{{ $data->product->expiry ?? '' }}</strong></label>
+                <label class="control-label">Expiry at : <strong>{{ $product_data->expiry ?? '' }}</strong></label>
             </div>
             <div class="control-group">
-                <label class="control-label">Added by user : <strong>{{ $data->product->user->name ?? '' }} </strong></label>
+                <label class="control-label">Added by user : <strong>{{ $product_data->user->name ?? '' }} </strong></label>
             </div>
             <div class="control-group">
-                <label class="control-label">Location : <strong>{{ $data->product->location->city->name ?? ''}} </strong></label>
+                <label class="control-label">Location : <strong>{{ $product_data->location->city->name ?? ''}} </strong></label>
+            </div>
+            @php $i = 1; @endphp
+            @foreach($product_fields_data as $data)
+            <div class="control-group">
+                <label class="control-label">Field [$i] : <strong>{{ $data->field->label ?? ''}} </strong></label>
             </div>
             <div class="control-group">
-                <label class="control-label">Field : <strong>{{ $data->field->label ?? ''}} </strong></label>
+                <label class="control-label">Value [$i]: <strong>{{ $data->value ?? ''}} </strong></label>
             </div>
-            <div class="control-group">
-                <label class="control-label">Value : <strong>{{ $data->value ?? ''}} </strong></label>
-            </div>
-            @csrf
+            @php $i++; @endphp
+            @endforeach
         </div>
       </div>
     </div>
