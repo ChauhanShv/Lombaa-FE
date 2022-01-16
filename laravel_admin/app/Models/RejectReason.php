6bb8f1cf55348c6d69b4cc9625da4b98292ac438
Rishabh Shukla <rishabh@datortech.com>
2022-01-17 00:02:23 +0530

    Reject reason CRUD done with changes

--- laravel_admin/app/Models/RejectReason.php
+++ laravel_admin/app/Models/RejectReason.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
-class Rejectreason extends Model
+class RejectReason extends Model
 {
     use HasFactory;
     use Uuids;
