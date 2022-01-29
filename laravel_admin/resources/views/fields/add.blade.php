85c96c0328614fa98df0b3bb21a1fdea3eba3f23
Rishabh Shukla <rishabh@datortech.com>
2022-01-29 17:40:49 +0530

    Field tag name change and error handling

--- laravel_admin/resources/views/fields/add.blade.php
+++ laravel_admin/resources/views/fields/add.blade.php
@@ -49,8 +49,8 @@ add_button {
             <div class="control-group">
               <label class="control-label">Field tag :</label>
               <div class="controls">
-                <input type="text" name="field_tag" value="{{ old('field_tag')}}" style="width: 40%" class="span11"  />
-                @error('field_tag')
+                <input type="text" name="tag" value="{{ old('tag')}}" style="width: 40%" class="span11"  />
+                @error('tag')
                   <div class="alert alert-danger " style="width: 34.2%">{{ $message }}</div>
                 @enderror
               </div>
