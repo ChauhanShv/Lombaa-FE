8b1ba461eb21a3c67012056eba6553458a209014
vivek <vivek@datortech.com>
2022-03-03 17:54:13 +0530

    issue in notification path resolved

--- node_api/src/notification/notification.service.js
+++ node_api/src/notification/notification.service.js
@@ -16,10 +16,11 @@ class NotificationService {
     async sendNotification(data) {
         const chat = await Chat.findOne({
             where: { id: data.ChatId }, include: [
-                { model: User, as: 'seller', attributes: ["name", "lastActiveAt"] },
-                { model: User, as: 'buyer', attributes: ["name"] },
+                { model: User, as: 'seller', attributes: ["id", "name", "lastActiveAt"] },
+                { model: User, as: 'buyer', attributes: ["id", "name"] },
             ]
         })
+        const postedById = data.postedBy.id
         const lastActive = chat.seller.lastActiveAt
         const Date = moment(lastActive)
         const current = moment()
@@ -27,10 +28,12 @@ class NotificationService {
         const userId = chat.sellerId
         if (diffDate <= 10)
             return data;
-
+        let path = `chat/sell/${chat.id}`
+        if (postedById === chat?.buyerId) {
+            path = `chat/buy/${chat.id}`
+        }
         const name = `New message from ${chat.buyer.name}`
         const description = data.text
-        const path = `chat/${data.ChatId}`
         const type = "chat"
         const notification = Notification.create({ text: name, description: description, path: path, type: type, userId: userId })
         return data
