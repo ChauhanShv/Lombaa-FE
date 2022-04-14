38010d167696fc6706b8eb4e6a66d61a4f5caadb
vivek <vivek@datortech.com>
2022-04-14 12:57:02 +0530

    TOken added in get by id product api

--- node_api/src/product/product.controller.js
+++ node_api/src/product/product.controller.js
@@ -22,6 +22,7 @@ const { productId } = require("../chat/chat.schema");
 const FavoriteProduct = require("../user/user.favorite_product_model");
 const SettingService = require("../settings/settings.service");
 const sequelize = require("../modules/sequelize/sequelize.service");
+const { findOne } = require("../file/file.model");
 
 
 
@@ -160,9 +161,18 @@ class ProductController extends BaseController {
       if (!singleProduct) {
         return super.jsonRes({ res, code: 200, data: { success: false, message: "Failed to get product", message_detail: "Product is unavailable" } })
       }
+      if (userId === singleProduct.userId) {
+
+        const productMedia = singleProduct.productMedia.forEach((media) => {
+          const payload = { userId: userId, fileId: media.fileId };
+          const token = jwt.encode(payload, "30d");
+          media.token = token;
+          media.setDataValue('token', token)
+
+        });
+      }
       if (userId) {
         singleProduct = (await this.service.mapUserFavorite([singleProduct], userId))?.[0];
-
         const viewed = viewedProduct.create({ userId: userId, productId: singleProduct.id })
       }
       const title = await this.service.fieldsMapping([singleProduct])
@@ -313,8 +323,9 @@ class ProductController extends BaseController {
 
       const productFieldData = req?.body?.fields?.map((field) => ({ fieldId: field?.id, value: field?.value?.value, fieldValueId: field?.value?.id, productId: data?.id }));
       await ProductField.update(productFieldData, { where: { productId: data?.id } });
+      const deleteMedia = ProductMedia.destroy({ fileId, where: { productId: productId } })
       const mediaList = req.body?.media?.map((media) => ({ fileId: media?.fileId, productId: data?.id, isPrimary: media?.isPrimary ?? false }));
-      await ProductMedia.update(mediaList, { where: { productId: data?.id } });
+      await ProductMedia.bulkCreate(mediaList, { where: { productId: data?.id } });
       return super.jsonRes({ res, code: 200, data: { success: true, messaage: "Product has been edited" } })
     }
     catch (error) {
