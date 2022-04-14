eeaeb91ea5480765266ddf5cc3f92c816f7dec37
--all <mufaddal.kanchwala@datortech.com>
2022-04-14 17:05:00 +0530

    Edit listing Successful tile bug

--- ui/src/components/edit-post/index.ts
+++ ui/src/components/edit-post/index.ts
@@ -1,6 +1,5 @@
 export * from './edit-post';
 export * from './drag-and-drop';
 export * from './edit-post-container';
-export * from './listing-successful-tile';
 export * from './edit-form-fields';
 export * from './edit-category-dropdown';
