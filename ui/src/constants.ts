eb9da5d42ff94f7290e7b91692d89e067b8bca63
Mufaddal <mufaddal.kanchwala@datortech.com>
2022-05-17 14:42:45 +0530

    Milestone 4 new bug fixes

--- ui/src/constants.ts
+++ ui/src/constants.ts
@@ -1,7 +1,7 @@
 // export const PASSWORD_REGEX: RegExp = /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/;
 export const PASSWORD_REGEX: RegExp = /^.{6,}$/;
 export const MOBILE_REGEX: RegExp =
-  /^[1-9][0-9]{9,14}$/;
+  /^[0-9][0-9]{9,14}$/;
 export const TIN_REGEX: RegExp = /^(?:\d{3}-\d{2}-\d{4}|\d{2}-\d{7})$/;
 export const NAME_MIN_LENGTH = 3;
 export const TIN_MIN_LENGTH = 6;
