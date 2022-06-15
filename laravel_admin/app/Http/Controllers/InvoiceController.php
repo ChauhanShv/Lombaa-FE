36b2655427bf1e2ddc14d9681e14cd5bc85e40e7
Ajay Nishad <ajay@datortech.com>
2022-06-15 17:28:50 +0530

    user invoice added

--- laravel_admin/app/Http/Controllers/InvoiceController.php
+++ laravel_admin/app/Http/Controllers/InvoiceController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Invoices;
+use App\Models\UserPackage;
+use Carbon\Carbon;
+use Str;
 
 class InvoiceController extends Controller
 {
@@ -13,7 +16,9 @@ class InvoiceController extends Controller
     }
 
     public function status_upated(Request $request, $id) {
+         $package_id = Invoices::where('id', $id)->select('packageId')->first();
          $uptade_invoice = Invoices::where([['id', $id],['status', 'unpaid']])->update(['status' => 'paid']);
+
          if($uptade_invoice) {
             return redirect()->back()->with('response', ['status' => 'success', 'message' => 'Invoice paid successfully ']);
          }
