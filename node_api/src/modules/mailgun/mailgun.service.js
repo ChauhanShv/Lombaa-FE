ff37755d39b4ad92496a33dc461f3c3ed82df482
vivek <vivek@datortech.com>
2022-06-19 14:43:23 +0530

    download pdf - done

--- node_api/src/modules/mailgun/mailgun.service.js
+++ node_api/src/modules/mailgun/mailgun.service.js
@@ -5,6 +5,5 @@ const Mailgun = require('mailgun.js');
 const mailgun = new Mailgun(formData).client({ username: config.username, key: config.apiKey, public_key: config.publicKey });
 
 exports.create = (data) => {
-    console.log({ data, domain: config.domain, username: config.username, key: config.apiKey, publicKey: config.publicKey });
     mailgun.messages.create(config.domain, data).then().catch(e => console.log(e));
 };
